0ab517da0939f818460aa68f91522eb0723e9d1a
Bartosz Stachowiak <sbartekt@op.pl>
2024-03-10 21:35:53 +0100

    delegate version management to setuptools_scm

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 from setuptools import setup, find_packages
 
-# get version from git tag
-from subprocess import check_output
-
-version = check_output(["git", "describe", "--tags"]).strip().decode().lstrip("v")
-
-setup(packages=find_packages(), install_requires=[], version=version)
+setup(packages=find_packages(), install_requires=[])
