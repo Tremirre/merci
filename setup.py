4c401aa9e00adbb142b49aa2709526ff5194c5be
Bartosz Stachowiak <sbartekt@op.pl>
2024-03-10 21:15:11 +0100

    add automatic version checking

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 # get version from git tag
 from subprocess import check_output
 
-version = check_output(["git", "describe", "--tags"]).strip().decode()
+version = check_output(["git", "describe", "--tags"]).strip().decode().lstrip("v")
 
-setup(packages=find_packages(), install_requires=[])
+setup(packages=find_packages(), install_requires=[], version=version)
